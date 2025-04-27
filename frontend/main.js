f75b94a3fb397a7a86a350c13eda20c81964d707
balota-natan-utcn <balotann61@gmail.com>
2025-04-27 16:46:46 +0300

    up

--- frontend/main.js
+++ frontend/main.js
@@ -1,10 +1,11 @@
 const gallery = document.getElementById('gallery');
 
-const images = ['sm64.jpg', 'mkart64.webp', 'nes_contra.webp']; // Your images here
+const images = ['n64_bomberman64.webp', 'n64_kirby64.webp', 'n64_marioparty3.webp', 'n64_papermario.webp', 'nes_megaman.webp', 'nes_smb3.webp', 'ps1_crash.webp', 'ps1_ff7.webp', 'ps1_mgear.webp', 'ps1_spyro.webp', 'mkart64.webp', 'nes_contra.webp']; // Your images here
 let imagesToLoad = images.length; // total number of images
 let imagesLoaded = 0; // counter
 
-images.forEach(imageName => {
+images.forEach(imageName =>
+{
   const img = document.createElement('img');
   img.dataset.lowres = `http://localhost:3000/images/${imageName}`;
   img.dataset.highres = `http://localhost:3000/images/high/${imageName}`;
@@ -12,9 +13,12 @@ images.forEach(imageName => {
   gallery.appendChild(img);
 });
 
-const observer = new IntersectionObserver((entries, observer) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
+const observer = new IntersectionObserver((entries, observer) =>
+{
+  entries.forEach(entry =>
+  {
+    if (entry.isIntersecting)
+    {
       const img = entry.target;
 
       img.src = img.dataset.lowres;
@@ -23,7 +27,8 @@ const observer = new IntersectionObserver((entries, observer) => {
         const highRes = new Image();
         highRes.src = img.dataset.highres;
 
-        highRes.onload = () => {
+        highRes.onload = () =>
+        {
           img.src = highRes.src;
           img.classList.remove('blur');
 
@@ -31,7 +36,8 @@ const observer = new IntersectionObserver((entries, observer) => {
           imagesLoaded++;
 
           // If all images are loaded, manually signal
-          if (imagesLoaded === imagesToLoad) {
+          if (imagesLoaded === imagesToLoad)
+          {
             console.log('All images loaded!');
             window.dispatchEvent(new Event('load'));
           }
@@ -41,13 +47,15 @@ const observer = new IntersectionObserver((entries, observer) => {
       observer.unobserve(img);
     }
   });
-}, {
+},
+{
   rootMargin: '100px',
   threshold: 0.1
 });
 
 // Observe all images
-document.querySelectorAll('#gallery img').forEach(img => {
+document.querySelectorAll('#gallery img').forEach(img =>
+{
   observer.observe(img);
 });
 
